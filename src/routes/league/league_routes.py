5d37375aa0f94eadf4f4718af1407c577188b1f1
Josuan Leonardo Hulom <hulomjosuan@gmail.com>
2025-07-08 13:11:48 +0800

    added route for resource

--- src/routes/league/league_routes.py
+++ src/routes/league/league_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from src.controllers.league_controllers import LeagueControllers
+from src.controllers.league_controllers import LeagueControllers, LeagueResourceController
 from src.controllers.league_categories import league_categories
 league_bp = Blueprint('league', __name__,url_prefix='/league')
 
@@ -9,7 +9,7 @@ leagueControllers = LeagueControllers()
 
 league_bp.get('/fetch')(leagueControllers.filter_leagues_by_organization_details)
 league_bp.get('/get-league-team/<string:league_team_id>')(leagueControllers.get_league_team)
-league_bp.get('/get-league-meta/<string:league_administrator_id>')(leagueControllers.fetch_league_meta)
+league_bp.get('/meta/<string:league_administrator_id>')(leagueControllers.fetch_league_meta)
 league_bp.get('/category-list')(league_categories)
 
 league_bp.post('/create-new')(leagueControllers.create_league)
@@ -21,3 +21,8 @@ league_bp.delete('/delete/<string:league_id>')(leagueControllers.delete_league)
 
 league_bp.patch('/accept-team')(leagueControllers.accept_team)
 league_bp.patch('/league-team/set-status/<string:league_team_id>')(leagueControllers.set_league_team_status)
+
+league_bp.get('/resource/<string:league_id>')(LeagueResourceController.get_league_resources)
+league_bp.post('/resource/create')(LeagueResourceController.create_league_resources)
+league_bp.put('/resource/update/<string:league_id>')(LeagueResourceController.update_league_resources)
+league_bp.delete('/resource/delete/<string:league_id>')(LeagueResourceController.delete_league_resources)
